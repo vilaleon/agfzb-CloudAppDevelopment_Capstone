3fbaaefb002e9ca1d6cbd6f4c3f020f3a72df5f6
vilaleon <>
2023-05-07 08:01:40 -0400

    Complete module 2 tasks

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+    path(route='', view=views.get_dealerships, name='index'),
+
     # path for about view
     path(route='about', view=views.about, name='about'),
 
@@ -16,12 +18,13 @@ urlpatterns = [
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='signup', view=views.registration_request, name='signup'),
 
     # path for login
+    path(route='login', view=views.login_request, name='login'),
 
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
